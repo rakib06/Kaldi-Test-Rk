525dc32731b20431aa1d5b7998eb914f4e58449c
Rakibul <rakibul@pop-os.localdomain>
2020-11-04 11:40:32 +0600

    Successfully run yesno

--- egs/yesno/s5/path.sh
+++ egs/yesno/s5/path.sh
@@ -1,4 +1,5 @@
 export KALDI_ROOT=`pwd`/../../..
+# export KALDI_ROOT='/home/dev/kaldi'
 [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
@@ -6,3 +7,5 @@ export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
 export LC_ALL=C
 
 
+# /home/rakibul/dev/kaldi/src/fstbin/fstaddselfloops.cc
+# export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
